bdffc52a5a17b97dcbe36e79e1d24bdbb9f9b12e
Либин Николай Павлович <N.Libin@isb>
2019-09-09 16:39:22 +0300

    First steps

---
+++ TinyBaseJS.js
@@ -0,0 +1,33 @@
+/*jshint esversion: 6 */
+/*jshint browser: true */
+
+class TinyDB {
+	constructor(base) {
+	  this.base = base;
+	}
+
+	getFromRelation (tableName, fieldName, key)  {
+		this.throwIfWrongRelation(tableName, fieldName);
+		let many = this.base.__relations[tableName][fieldName].to === "many";
+
+		return this.get(tableName, fieldName, key, many);
+	}
+
+	get(tableName, fieldName, key, all = false) {
+		if (key instanceof Array) return key.map( i => this.get(tableName, fieldName, i) );
+		let fn = all ? "filter" : "find";
+
+		return key instanceof Function ?
+			base[tableName][fn]( entry => key(entry) )
+		:
+			base[tableName][fn]( entry => entry[fieldName] === key )
+		;
+	}
+
+	throwIfWrongRelation(tableName, fieldName) {
+		let base = this.base;
+		if ( !(base.__relations && base.__relations[tableName] && base.__relations[tableName][fieldName]) ) {
+			throw new Error(`There is not relation's description in base.__relations. Table: ${tableName}, field: ${fieldName}`);
+		}
+	}
+}
