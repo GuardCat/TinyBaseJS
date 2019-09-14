726e5127ca90997de984fc103493c4622435343c
GuardCat <storktec@mail.ru>
2019-09-14 20:57:50 +0300

    Вернул работу на место

--- TinyBaseJS.js
+++ TinyBaseJS.js
@@ -1,95 +1,107 @@
-/*jshint esversion: 6 */
-/*jshint browser: true */
+/*jshint camelcase: true, esnext: true, browser: true, browserify: true*/
 
-/** 
+/**
  * @class
  * @classdesc simple Database operator for existance DB in browser. Data format see in README.MD
- * @param base Object — formatted object. 
+ * @param {Object} base — formatted object with __structure entry.
  */
-	
+
 
 class TinyDB {
-	constructor(base) {
-	  this.base = base;
+	constructor(base = { __structure: {} }) {
+		this.base = base;
+		this.__structure = base.__structure;
 	}
 
-	getFromRelation (tableName, fieldName, key)  {
-		this.throwIfNoRelation(tableName, fieldName);
-		let rel = this.base.__relations[tableName][fieldName];
+	
+	/**
+ 	* @description getLinkValue method Возвращает значение по полю со ссылкой и его значению
+	* @param {string} tableName имя таблицы, для которой будем искать ссылку
+	* @param {string} fieldName имя поля в таблице, содержащее ключи для ссылки
+	* @param {string/integer | array of strings/integers} Ключ или массив ключей, по которым будут возвращены значения.
+	* @param {array | boolean} массив полей, которые возвращаются из целевой таблицы. Если отсутствует, возвращается вся запись.
+ 	*/
+	getLinkValue (tableName, fieldName, key, getFields = false)  {
+		if (key instanceof Array) return key.map( key => this.getLinkValue(tableName, fieldName, key, getFields) );
+		this.throwIfWrongLink(tableName, fieldName);
 
-		return this.get(rel.toTable, rel.byField, key, rel.to === "many");
-	}
-
-	get(tableName, fieldName, key, all = false) {
-		if (key instanceof Array) return key.map( i => this.get(tableName, fieldName, i) );
-		let fn = all ? "filter" : "find";
-		
-		return key instanceof Function ?
-			this.base[tableName][fn]( entry => key(entry) )
-		:
-			this.base[tableName][fn]( entry => entry[fieldName] === key )
+		let
+			link = this.getStruct(tableName, fieldName),
+			finder = link.to === "many" ? "filter" : "find",
+			result = this.base[link.toTable][finder](i => key === i[link.byField])
 		;
+
+		if (!getFields) return result;
+		if ( !(getFields instanceof Array) ) throw new ReferenceError("getLinkValue: the getFields argument must be an array if it is present.");
+		if (getFields.length > 1) return getFields.reduce( (a, b) => { a[b] = result[b]; return a }, { } );
+		return result[getFields];
 	}
-	
-	delRel(tableName, fieldName) {
-		let rel = this.base.__relations[tableName];
-		if ( !rel || !rel[fieldName] ) return false;
-		return delete rel[fieldName];
+
+	getStruct(tableName, fieldName) {
+		this.throwIfNoField(tableName, fieldName);
+		return this.__structure[tableName].find(i => i.name === fieldName);
 	}
-	
-	addRel(tableName, fieldName, relationOBJ) {
-		if (!this.base.__relations) this.base.__relations = {};
-		let	rel = this.base.__relations;
-		if (!rel[tableName]) rel[tableName] = {};
-		if (rel[tableName][fieldName]) throw new Error(`The relation exists: ${rel[fieldName]}. Delete it first and try again or use chRel method.`);
-		
-		rel[tableName][fieldName] = relationOBJ;
-		
-		return rel[tableName][fieldName];
-	}
-	
-	del(tableName, fieldName, key) {
-		if (key instanceof Array) return key.map( i => this.del(tableName, fieldName, i) );
-		
-		if (!fieldName) {
-			this.throwIfExistsRefsToTable(tableName);
-			return delete this.base[tableName];
-		}
-		
-		this.base = key instanceof Function ?
-			this.base[tableName].filter( entry => key(entry) )
-		:
-			this.base[tableName].filter( entry => entry[fieldName] === key )
-		;
-		
-		return this;		
-	}
-	
-	throwIfWrongRelation(relObj) {
-		
-	}
-	
-	throwIfNoRelation(tableName, fieldName) {
-		let rel = this.base.__relations;
-		if ( !(rel && rel[tableName] && rel[tableName][fieldName]) ) {
-			throw new Error(`There is not relation's description in base.__relations. Table: ${tableName}, field: ${fieldName}`);
-		}
-	}
-	
-	throwIfExistsRefsToTable(tableName) {
-		let rel = this.base.__relations;
-		if ( !(rel) ) return false;
-		
-		for (let table in rel) {
-			if ( !(rel.hasOwnProperty(table)) || table === tableName ) continue;
-			
-			for (let field in rel[table]) {
-				if ( !(rel[table].hasOwnProperty(field) ) ) continue;
-				if (rel[table][field].toTable === tableName) throw new Error(`Operation not permitted. There is a relation to ${tableName}, in table ${table}: ${JSON.stringify(rel[table][field])}. Delete relation first and try again.`);
+
+	/**
+	* @description it replaces links in table to values
+	* @param table an Array of Objects in TinyBaseJs format.
+	* @param tableName a string, that contains name of the table in the base with the same rules links.
+	* @param {object | boolean} объект массивов полей, которые возвращаются из целевой таблицы. Ключи объекта — названия полей исходной таблицы
+	*   содержимое — массив имен полей целевой таблицы к передаче. Если параметр отсутствует, возвращается вся запись.
+	*/
+	mergeLinks(table, tableName, getFields = false) {
+		return  table.map(
+			row => {
+				let result = {};
+				for (let fieldName in row) {
+					if ( !row.hasOwnProperty(fieldName) ) continue;
+					this.throwIfNoField(tableName, fieldName);
+					
+					result[fieldName] = this.getStruct(tableName, fieldName).type === "link" ? this.getLinkValue(tableName, fieldName, row[fieldName], getFields[fieldName]) : row[fieldName];
+				}
+
+				return Object.assign({ }, result);
 			}
-		
-		}
-		
+		);
 	}
-	
+
+	/* throwIf zone */
+	throwIfNoTable(tableName) {
+		if ( !this.__structure[tableName] ) throw new ReferenceError(`There is not table "${tableName}" here.`);
+	}
+
+	throwIfNoField(tableName, fieldName) {
+		this.throwIfNoTable(tableName);
+		if ( !this.__structure[tableName].find(i => i.name === fieldName) ) throw new ReferenceError(`There is not field "${fieldName}" in table "${tableName}".`);
+	}
+
+	throwIfNoFieldType(tableName, fieldName) {
+		this.throwIfNoField(tableName, fieldName);
+		if ( !this.__structure[tableName].find(i => i.name === fieldName).type ) throw new Error(`There is not "type" field for field "${fieldName}" in table "${tableName}". It's need to fix it.`);
+	}
+
+	throwIfNoLink(tableName, fieldName) {
+		this.throwIfNoFieldType(tableName, fieldName);
+		if ( this.__structure[tableName].find(i => i.name === fieldName).type !== "link" ) throw new TypeError(`Field "${fieldName}" in table "${tableName}" is not link.`);
+	}
+
+	throwIfWrongLink(tableName, fieldName, key) {
+		this.throwIfNoLink(tableName, fieldName);
+		let link = this.getStruct(tableName, fieldName);
+		if( !this.base[link.toTable].some( i => i[fieldName] === key) ) throw new ReferenceError(`Value "${key}" doesn't exist in table linked to field "${fieldName}" in table ${tableName}`);
+	}
+
 }
+
+/**
+* Методы, которые реально нужны:
+* - del. Чтобы исключить удаление строк и таблиц,
+*   ломающее ссылки.
+* - change. Чтобы исключить смену значений на
+*   некорректные: id больше следующего, несуществующее
+*   значение ссылки, утрату значений, на которые ссылаются.
+* - renameField
+* - add
+* + getLinkValue
+* - __generateLinksObject
+**/
